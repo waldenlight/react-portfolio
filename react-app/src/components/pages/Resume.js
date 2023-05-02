0df157ced1922d7132ef099df9e75f2bd33708e0
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-05-02 09:59:34 -0600

    Edit resume styling

--- react-app/src/components/pages/Resume.js
+++ react-app/src/components/pages/Resume.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+const styles = {
+    mainDivStyles: {
+        margin: "20px 150px",
+        padding: '20px 30px',
+        backgroundColor: '#A569BD',
+        color: 'white'
+    },
+    anchorStyles: {
+        color: 'white'
+    },
+    imageStyle: {
+        width: '300px',
+        margin: '10px 0px',
+        border: '6px solid #A569BD'
+    }
+};
+
 export default function Resume() {
     return (
-        <div>
-            <h1><a href='https://docs.google.com/document/d/1GSe6eq7my1bz4G9xNdjcqu5d4ITHPUhMZL1B0gTZp-s/edit?usp=sharing'>Resume</a></h1>
+        <div style={styles.mainDivStyles}>
+            <h1><a style={styles.anchorStyles} href='https://docs.google.com/document/d/1GSe6eq7my1bz4G9xNdjcqu5d4ITHPUhMZL1B0gTZp-s/edit?usp=sharing'>Resume</a></h1>
             <p>
                 Full-stack web developer seeking an entry-level role as a junior software engineer or web developer.  Pursuing a degree in computer science from the University of Colorado and will graduate in Spring 2026. Also, earning a certificate in full stack web development from the University of Utah Professional Education Coding Boot Camp that will be completed in May 2023. Passionate about software and willing to complete any task where I can contribute to the team.
             </p>
