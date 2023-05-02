23dc7d32ec477d6da6176d91db4cf95591013e47
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-05-02 09:19:25 -0600

    Edit footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -5,8 +5,27 @@ const styles = {
         position: 'fixed',
         left: '0',
         bottom: '0',
-        width: '100%'
+        width: '100%',
+        backgroundColor: '#0B196A',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px'
     },
+    ulStyles: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    liStyles: {
+        margin: '0px 10px',
+        listStyleType: 'none',
+    },
+    anchorStyles: {
+        textDecoration: 'none',
+        color: 'white',
+    }
 };
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
@@ -14,8 +33,13 @@ const styles = {
 function Footer({ currentPage }) {
     return (
         <div style={styles.footerStyle}>
-            <h4>{currentPage}</h4>
+            <h4>{currentPage} Page</h4>
             <h5>Walden Light</h5>
+            <div style={styles.ulStyles}>
+                <p style={styles.liStyles}><a style={styles.anchorStyles} href='https://github.com/waldenlight'>GitHub</a></p>
+                <p style={styles.liStyles}><a style={styles.anchorStyles} href='https://www.linkedin.com/in/walden-light-102a3321a/'>LinkedIn</a></p>
+                <p style={styles.liStyles}><a style={styles.anchorStyles} href='mailto:waldenlight@gmail.com'>Email</a></p>
+            </div>
         </div>
     );
 }
