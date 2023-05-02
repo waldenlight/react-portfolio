3d98fec3115781d052d6358a358f625950c71d94
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-05-02 08:55:26 -0600

    Edit resume page

--- react-app/src/components/pages/Resume.js
+++ react-app/src/components/pages/Resume.js
@@ -3,10 +3,12 @@ import React from 'react';
 export default function Resume() {
     return (
         <div>
-            <h1>Resume</h1>
+            <h1><a href='https://docs.google.com/document/d/1GSe6eq7my1bz4G9xNdjcqu5d4ITHPUhMZL1B0gTZp-s/edit?usp=sharing'>Resume</a></h1>
             <p>
-                Add resume
+                Full-stack web developer seeking an entry-level role as a junior software engineer or web developer.  Pursuing a degree in computer science from the University of Colorado and will graduate in Spring 2026. Also, earning a certificate in full stack web development from the University of Utah Professional Education Coding Boot Camp that will be completed in May 2023. Passionate about software and willing to complete any task where I can contribute to the team.
             </p>
+            <h2>Skills</h2>
+            <p>JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, React, Node, Handlebars, jQuery, Bootstrap, Python, Django</p>
         </div>
     );
 }
